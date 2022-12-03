94b4adfc2a615b93c90fff094c19fc2be35c9a90
Stanley Chu <chustanleys@gmail.com>
2022-12-03 12:36:10 -0800

    Complete N-Rooks

--- src/solvers.js
+++ src/solvers.js
@@ -16,41 +16,64 @@
 window.findNRooksSolution = function(n) {
   var solution = new Board({'n': n});
   var rooksArray = solution.rows();
-
   for (var i = 0; i < rooksArray.length; i++) {
     for (var j = 0; j < rooksArray[i].length; j++) {
       solution.togglePiece(i, j);
-
       if (solution.hasRowConflictAt(i) || solution.hasColConflictAt(j)) {
         solution.togglePiece(i, j);
       }
-
     }
-
   }
-
-  /*
-  for every index, toggle.
-  check to see if it has any row or column conflicts.
-  if it does, change the toggled index back to 0, and move on
-  */
-
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(rooksArray));
   return rooksArray;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var solution = new Board({'n': n});
+
+   /*
+  var matrix = [
+
+        0   1   2.  3
+
+        [1, 0, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1], //2
+        [0, 0, 0, 0]
+      ];
+  */
+
+  var recursiveFunc = function (currentRow) {
+    if (currentRow === n) {
+      solutionCount++;
+      return;
+    }
+
+    // base case -- what ends the recursive function
+    for (var i = 0; i < n; i++) { // 2
+      solution.togglePiece(currentRow, i);
+      if (!solution.hasAnyRooksConflicts()) { // if it doesnt have any conflicts
+        recursiveFunc(currentRow + 1); // then move on to next row
+      }
+
+      solution.togglePiece(currentRow, i);
+
+    }
+  };
+
+  recursiveFunc(0);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
+
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   var solution = new Board({'n': n});
-  var rooksArray = solution.rows();
+  var queensArray = solution.rows();
   var conflictFlag = false;
   /*
   var matrix = [
@@ -64,50 +87,99 @@ window.findNQueensSolution = function(n) {
       ];
   */
 
-  //n = 4
-  //currentRow = 4
-
   var recursiveFunc = function (currentRow) {
-
+    // base case -- what ends the recursive function
     if (currentRow === n) {
       conflictFlag = true;
+      // when this returns, it means it has reached the end of the chessboard.
       return;
     }
 
-    // base case -- what ends the recursive function
-    for (var i = 0; i < n; i++) { // 2
-      solution.togglePiece(currentRow, i); // adds 1 into [[1]]
-
-      // if it doesnt have any conflicts
-      if (!solution.hasAnyQueensConflicts()) {
-        // then move on to next row
-        recursiveFunc(currentRow + 1);
-        //entering 2nd row
-        //entering 3rd row
-        //entering 4th row
+    // for loop for the column
+    for (var i = 0; i < n; i++) {
+      // always adding chess pieces '1' on the board
+      solution.togglePiece(currentRow, i);
+      // checks if it has conflicts.
+      if (!solution.hasAnyQueensConflicts()) { // if it doesnt have any conflicts
+        var nextStep = currentRow + 1;
+        recursiveFunc(nextStep); // then move on to next row
       }
-
+      // will only return true if you have reached the end of the chessboard.
       if (conflictFlag) {
+        // will leave the function if true
         return;
       } else {
+        // if there is a conflict(previous if statement) and conflict flag is NOT true, delete the chess piece that was just added at row 89.
         solution.togglePiece(currentRow, i);
       }
-
-
     }
-    return; // done with loop on 3rd row so returns and goes back to the 2nd row
+    return;
   };
-
+  //Calling the recursive function with 0 because our input for our recursiveFunc is our 'rows' and our 'column' is the i in the for loop within the recursiveFunc
   recursiveFunc(0);
 
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(rooksArray));
-  return rooksArray;
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(queensArray));
+  return queensArray;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var solution = new Board({'n': n});
+ /*
+  var matrix = [
+
+        0   1   2.  3
+
+        [1, 0, 0, 0],
+        [0, 0, 1, 1],
+        [0, 0, 0, 0], //2
+        [0, 0, 0, 0]
+      ];
+  */
+
+  if (n === 2 || n === 3) {
+    return solutionCount;
+  }
+
+  var recursiveFunc = function (currentRow) {
+
+    // base case -- what ends the recursive function
+    // asssuming that each solution is correct, add one to the scoreboard
+    if (currentRow === n) {
+      solutionCount++;
+      return;
+    }
+
+
+    for (var i = 0; i < n; i++) {
+      //creating chess piece at every index on currentRow
+      solution.togglePiece(currentRow, i);
+
+      //IF THERE IS A CONFLICT
+      if (solution.hasAnyQueensConflicts()) {
+        //DELETE THE 'JUST-ADDED' CHESS PIECE ON LINE 157
+        solution.togglePiece(currentRow, i);
+      //IF THERE IS NO CONFLICT
+      } else {
+        //MOVE TO THE NEXT ROW
+        recursiveFunc(currentRow + 1);
+        //WHEN WE GO BACK TO EXECUTION CONTEXT, WE DELETE THE CHESS PIECE BECAUSE
+        // 1. we either went back because we found our solution and want to find more (basecase activated)
+
+        //2. we have no chess piece on a specific row, loop ended and it exits us out of our execution context bringing the 'interpreter' back to line 167 to continue the rest of the recursion.
+        solution.togglePiece(currentRow, i);
+
+      }
+
+
+
+    }
+
+  };
+
+  recursiveFunc(0);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
